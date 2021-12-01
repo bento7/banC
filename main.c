ae251012304b7006bb42614387af257f02a7bd60
Thomas <thomas.albert@ensta-bretagne.org>
2021-12-01 23:22:37 +0100

    Ajout des fonctions demandées, non terminées

--- main.c
+++ main.c
@@ -12,16 +12,16 @@ struct Date
 struct Transaction
 {
     struct Date date;
-    int amount;
-    char name_giver [32];
-    char name_receiver [32];
+    float amount;
+    char label [30];
+    char name [32]; // émetteur si transaction > 0 :réception d'€ | receveur si transaction < 0: envoi d'€
 };
 
 // create struct Entete
 struct Entete
 {
     struct Date date;
-    int solde;
+    float solde;
 };
 
 // create struct Account
@@ -47,6 +47,46 @@ void date(struct Date *d) {
     d->month = local->tm_mon + 1;      // get month of year (0 to 11)
     d->year = local->tm_year + 1900;   // get year since 1900
 }
+
+void ouvrir(FILE **f, char nom[]){
+    //qui ouvre le fichier donné ou le crée sinon.
+
+    // si File n'existe pas
+        // Créé file
+
+    // Dans tous les cas
+        // fopen(file);
+
+};
+
+void fermer(FILE*f){
+    fclose(f);
+}
+
+struct Entete creation_entete(struct Date d, float solde){
+    struct Entete e1;
+    e1.date = d;
+    e1.solde = solde;
+    return e1;
+};
+
+struct Transaction creation_transaction(struct Date d, float amt, char label, char name){
+    struct Transaction t1;
+    t1.date = d;
+    t1.amount = amt;
+    t1.label = label;
+    t1.name = name;
+    return t1;
+};
+
+int ajout_transaction(FILE* fp, *transaction){
+    // note : use fwrite() fonction
+
+    fp = fopen( "file.txt" , "w" );
+    fwrite(transaction , 1 , sizeof(transaction) , fp);
+
+    return *transaction;
+};
 
 int main() {
     struct Date d;
