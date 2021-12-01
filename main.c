d18dccdc6be904056c8564d388a95409c09d4365
Thomas <thomas.albert@ensta-bretagne.org>
2021-12-01 15:02:16 +0100

    Premier ajout

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+
+// create struct Transaction
+struct Transaction
+{
+    int date;
+    int amount;
+    char name_giver [32];
+    char name_receiver [32];
+};
+
+// create struct Date
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+// create struct Account
+struct Account
+{
+    int id;
+    char name [32];
+};
+
+void date(struct Date *d) {
+    // `time_t` is an arithmetic time type
+    time_t now;
+
+    // Obtain current time
+    // `time()` returns the current time of the system as a `time_t` value
+    time(&now);
+
+    // localtime converts a `time_t` value to calendar time and
+    // returns a pointer to a `tm` structure with its members
+    // filled with the corresponding values
+    struct tm *local = localtime(&now);
+
+    d->day = local->tm_mday;            // get day of month (1 to 31)
+    d->month = local->tm_mon + 1;      // get month of year (0 to 11)
+    d->year = local->tm_year + 1900;   // get year since 1900
+}
+
+int main() {
+    struct Date *d1;
+    date(d1);
+    printf("%i", d1->day);
+    printf("%i", d1->month);
+    return 0;
+}
